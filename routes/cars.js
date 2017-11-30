3038974094711acb6f434fa805dfab18d99e9919
JEMD <x_jemd_x@hotmail.com>
2017-11-30 01:14:17 -0300

    ruta de la imagen de los autos

--- routes/cars.js
+++ routes/cars.js
@@ -1,19 +1,19 @@
 var cars = [
     // Automóviles
-    {"make": "Suzuki", "model": "Alto 800", "color": "Green", "price": "250", "type": "car"},
-    {"make": "Hyundai", "model": "Elantra Coupe", "color": "White", "price": "250", "type": "car"},
-    {"make": "Volkswagen", "model": "Gol Trend", "color": "Red", "price": "250", "type": "car"},
+    {"make": "Suzuki", "model": "Alto 800", "color": "Green", "price": "250", "type": "car", "img": "/public/cars/Suzuki-Alto800.jpg"},
+    {"make": "Hyundai", "model": "Elantra Coupe", "color": "White", "price": "250", "type": "car", "img": "public/cars/Hyundai-Elantra-Coupe.jpg"},
+    {"make": "Volkswagen", "model": "Gol Trend", "color": "Red", "price": "250", "type": "car", "img": "public/cars/Volkswagen Gol Trend.jpgpublic/cars/Volkswagen Gol Trend.jpg"},
     // Jeeps
-    {"make": "Jeep", "model": "Renegade WagenClub", "color": "Blue", "price": "250", "type": "jeep"},
-    {"make": "Honda", "model": "Civic", "color": "Red", "price": "250", "type": "jeep"},
+    {"make": "Jeep", "model": "Renegade WagenClub", "color": "Blue", "price": "250", "type": "jeep", "img": "public/jeeps/Jeep-Renegade_WagenClub.jpg"},
+    {"make": "Jeep", "model": "Wrangler Unlimited", "color": "Black", "price": "250", "type": "jeep", "img": "public/jeeps/Jeep-Wrangler_Unlimited.jpg"},
     // Station wagons
-    {"make": "Fiat", "model": "Palio Weekend", "color": "White", "price": "250", "type": "station wagon"},
-    {"make": "Honda", "model": "Civic", "color": "Black", "price": "250", "type": "station wagon"},
-    {"make": "Volvo", "model": "V70", "color": "Grey", "price": "250", "type": "station wagon"},
+    {"make": "Fiat", "model": "Palio Weekend", "color": "White", "price": "250", "type": "station wagon", "img": "public/station-wagons/Fiat-Palio_Weekend.jpg"},
+    {"make": "Honda", "model": "Civic", "color": "Black", "price": "250", "type": "station wagon", "img": "public/station-wagons/Honda-Civic.jpg"},
+    {"make": "Volvo", "model": "V70", "color": "Grey", "price": "250", "type": "station wagon", "img": "public/station-wagons/Volvo-V70.jpg"},
     // Camionetas
-    {"make": "Ford", "model": "Ranger", "color": "Blue", "price": "250", "type": "van"},
-    {"make": "Dodge", "model": "Ram", "color": "Grey", "price": "250", "type": "van"},
-    {"make": "Toyota", "model": "DMAX", "color": "Red", "price": "250", "type": "van"}
+    {"make": "Ford", "model": "Ranger", "color": "Blue", "price": "250", "type": "van", "img": "public/vans/Ford-Ranger.jpg"},
+    {"make": "Dodge", "model": "Ram", "color": "Grey", "price": "250", "type": "van", "img": "public/vans/Dodge-Ram.jpg"},
+    {"make": "Toyota", "model": "DMAX", "color": "Red", "price": "250", "type": "van", "img": "public/vans/Toyota-DMAX.jpg"}
 ];
 
 exports.getCars = function (req, res) {
